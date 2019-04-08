d98608163261e36b7f295c69af9b932dfc31df56
Emanuele <bartolesiemanuele@hotmail.com>
2019-04-08 10:35:23 +0200

    first version

---
+++ SonequaBot/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SonequaBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TwitchChatBot bot = new TwitchChatBot();
+            bot.Connect();
+
+            Console.ReadLine();
+
+            bot.Disconnect();
+        }
+    }
+}
