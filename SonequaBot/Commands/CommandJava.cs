9147280704dd6ff4275e635e70fe3eb5592d4036
fdanti@gmail.com <fdanti@gmail.com>
2020-09-20 07:06:36 +0200

    Add Command Java

---
+++ SonequaBot/Commands/CommandJava.cs
@@ -0,0 +1,17 @@
+using SonequaBot.Commands.Interfaces;
+using SonequaBot.Commands.Interfaces.Responses;
+using TwitchLib.Client.Events;
+
+namespace SonequaBot.Commands
+{
+    public class CommandJava : CommandBase, IResponseMessage
+    {
+        protected new string Command = " java ";
+        protected new CommandActivationComparison ActivationComparison = CommandActivationComparison.Contains;
+        
+        public string GetMessage(OnMessageReceivedArgs e)
+        {
+            return $"Hey { e.ChatMessage.DisplayName }! Dillo ancora se hai coraggio!!!";
+        }
+    }
+}
