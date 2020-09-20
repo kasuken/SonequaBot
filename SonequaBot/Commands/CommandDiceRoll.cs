d9d099a55bfc79434ac7867f560315f0e591ac79
fdanti@gmail.com <fdanti@gmail.com>
2020-09-20 11:32:30 +0200

    Small fix

--- SonequaBot/Commands/CommandDiceRoll.cs
+++ SonequaBot/Commands/CommandDiceRoll.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Text.RegularExpressions;
-using SonequaBot.Commands.Interfaces;
 using SonequaBot.Commands.Interfaces.Responses;
 using TwitchLib.Client.Events;
 
@@ -8,22 +7,22 @@ namespace SonequaBot.Commands
 {
     public class CommandDiceRoll : CommandBase, IResponseMessage
     {
-        protected string Command = "!roll{@int dice faces}";
-
-        private int _defaultDiceFaces = 6;
         private readonly Random _rnd = new Random();
 
-        public override bool IsActivated(string message)
-        {
-            return base.IsActivated(message.Substring(0, 5));
-        }
+        private const int DefaultDiceFaces = 6;
+        protected string Command = "!roll{@int dice faces}";
 
         public string GetMessage(OnMessageReceivedArgs e)
         {
             var match = Regex.Match(e.ChatMessage.Message, "^!roll([0-9]+)");
-            var diceFaces = !match.Success ? _defaultDiceFaces : int.Parse(match.Groups[1].Value);
+            var diceFaces = !match.Success ? DefaultDiceFaces : int.Parse(match.Groups[1].Value);
 
             return $"{e.ChatMessage.DisplayName} roll a {_rnd.Next(1, diceFaces)} (1d{diceFaces})";
+        }
+
+        public override bool IsActivated(string message)
+        {
+            return base.IsActivated(message.Substring(0, 5));
         }
     }
 }
