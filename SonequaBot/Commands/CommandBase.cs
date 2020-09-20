9a8549efb164162085553bb62dd9d2d0188f52cd
fdanti@gmail.com <fdanti@gmail.com>
2020-09-20 07:05:46 +0200

    Enriched Base class and add more meaningful names to interface method.

--- SonequaBot/Commands/CommandBase.cs
+++ SonequaBot/Commands/CommandBase.cs
@@ -1,20 +1,45 @@
+using System;
+using System.Runtime.CompilerServices;
 using SonequaBot.Commands.Interfaces;
 
 namespace SonequaBot.Commands
 {
     public abstract class CommandBase :ICommand
     {
-        protected string Command;
-        protected CommandActivationComparison ActivationComparison;
+        /**
+         * The Command string for activation. 
+         */
+        protected string ActivationCommand;
+        
+        /**
+         * The Comparison type for activation. 
+         */
+        protected CommandActivationComparison ActivationComparison = CommandActivationComparison.StartsWith;
+
+        public bool IsActivated(string message)
+        {
+            switch (GetComparisonMethod())
+            {
+                case CommandActivationComparison.StartsWith:
+                    return message.StartsWith(GetActivationCommand(), StringComparison.InvariantCultureIgnoreCase);
+                    break;
+                
+                case CommandActivationComparison.Contains:
+                    return message.Contains(GetActivationCommand(), StringComparison.InvariantCultureIgnoreCase);
+                    break;
+            }
+
+            return false;
+        }
 
         public CommandActivationComparison GetComparisonMethod()
         {
             return ActivationComparison;
         }
 
-        public string GetCommand()
+        public string GetActivationCommand()
         {
-            return Command;
+            return ActivationCommand;
         }
     }
 }
--- SonequaBot/Commands/Interfaces/ICommand.cs
+++ SonequaBot/Commands/Interfaces/ICommand.cs
@@ -7,17 +7,22 @@ namespace SonequaBot.Commands.Interfaces
         StartsWith = 0,
         Contains = 1
     };
-    
+
     public interface ICommand
     {
+        /**
+         * Return true if command is triggered. 
+         */
+        bool IsActivated(string message);
+
         /**
          * Get activation string.
          */
         CommandActivationComparison GetComparisonMethod();
-        
+
         /**
          * Get activation string.
          */
-        string GetCommand();
+        string GetActivationCommand();
     }
 }
