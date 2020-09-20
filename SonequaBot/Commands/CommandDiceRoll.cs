ce419694f27f5fb4e96482cbe1d9d7231f585408
fdanti@gmail.com <fdanti@gmail.com>
2020-09-20 11:12:02 +0200

    Simplify DiceRoll

--- SonequaBot/Commands/CommandDiceRoll.cs
+++ SonequaBot/Commands/CommandDiceRoll.cs
@@ -8,29 +8,22 @@ namespace SonequaBot.Commands
 {
     public class CommandDiceRoll : CommandBase, IResponseMessage
     {
-        protected string Command = "!roll{dice-faces}";
+        protected string Command = "!roll{@int dice faces}";
 
-        private int _diceFaces = 6;
+        private int _defaultDiceFaces = 6;
         private readonly Random _rnd = new Random();
-        
+
         public override bool IsActivated(string message)
         {
-            _diceFaces = 6; // reset dice faces after a roll
-            
-            var activated = message.StartsWith("!roll", StringComparison.InvariantCultureIgnoreCase);
-            if (!activated) return false;
-
-            var match = Regex.Match(message, "^!roll([0-9]+)");
-            if (!match.Success) return true;
-
-            _diceFaces = int.Parse(match.Groups[1].Value);
-
-            return true;
+            return base.IsActivated(message.Substring(0, 5));
         }
 
         public string GetMessage(OnMessageReceivedArgs e)
         {
-            return $"{e.ChatMessage.DisplayName} roll a {_rnd.Next(1, _diceFaces)} (1d{_diceFaces})";
+            var match = Regex.Match(e.ChatMessage.Message, "^!roll([0-9]+)");
+            var diceFaces = !match.Success ? _defaultDiceFaces : int.Parse(match.Groups[1].Value);
+
+            return $"{e.ChatMessage.DisplayName} roll a {_rnd.Next(1, diceFaces)} (1d{diceFaces})";
         }
     }
 }
