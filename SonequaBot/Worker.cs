01abbacc99320f4b65455b5a1ecd323ec97eb5df
Emanuele Bartolesi <bartolesiemanuele@hotmail.com>
2020-09-26 11:25:32 +0200

    add appsetings, remove resx, add log

--- SonequaBot/Worker.cs
+++ SonequaBot/Worker.cs
@@ -1,17 +1,15 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using SonequaBot.Commands;
 using SonequaBot.Commands.Interfaces;
 using SonequaBot.Commands.Interfaces.Responses;
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
 using TwitchLib.Api;
 using TwitchLib.Client;
-using TwitchLib.Client.Events;
 using TwitchLib.Client.Models;
 
 namespace SonequaBot
@@ -20,24 +18,28 @@ namespace SonequaBot
     {
         private readonly ILogger<Sonequa> _logger;
 
-        readonly ConnectionCredentials connectionCredentials = new ConnectionCredentials(TwitchInfo.BotUsername, TwitchInfo.BotToken);
-        TwitchClient client;
+        readonly ConnectionCredentials connectionCredentials;
+        private readonly TwitchClient client = new TwitchClient();
         readonly TwitchAPI twitchAPI = new TwitchAPI();
 
-        string[] BotUsers = new string[] { "sonequabot", "streamelements" };
-        
-        Dictionary<string,ConnectedUser> ConnectedUsers = new Dictionary<string, ConnectedUser>();
-        
+        private readonly string[] BotUsers = new string[] { "sonequabot", "streamelements" };
+
+        Dictionary<string, ConnectedUser> ConnectedUsers = new Dictionary<string, ConnectedUser>();
+
         List<ICommand> BotCommands = new List<ICommand>();
+        private readonly HubConnection connection;
 
-        HubConnection connection;
+        private readonly SonequaSettings _options;
 
-        public Sonequa(ILogger<Sonequa> logger)
+        public Sonequa(ILogger<Sonequa> logger, SonequaSettings options)
         {
             _logger = logger;
+            _options = options;
+
+            connectionCredentials = new ConnectionCredentials(_options.BotUsername, _options.BotToken);
 
             connection = new HubConnectionBuilder()
-                .WithUrl("https://kasukensonequabotweb.azurewebsites.net/sonequaBotHub")
+                .WithUrl(_options.SonequaWebUrl)
                 .Build();
 
             connection.Closed += async (error) =>
@@ -56,19 +58,27 @@ namespace SonequaBot
 
         internal void Connect()
         {
-            Console.WriteLine("Connecting...");
+            _logger.LogInformation("Connecting...");
 
-            twitchAPI.Settings.ClientId = TwitchInfo.ClientId;
-            
+            twitchAPI.Settings.ClientId = _options.ClientId;
+
             InitializeBotCommands();
             InitializeBot();
         }
 
+        private void InitializeBotCommands()
+        {
+            //BotCommands.Add(new CommandHi()); Not needed in twitch
+            BotCommands.Add(new CommandJava()); ;
+            BotCommands.Add(new CommandPhp());
+            BotCommands.Add(new CommandDevastante());
+            BotCommands.Add(new CommandSlap());
+            BotCommands.Add(new CommandDiceRoll());
+        }
+
         private void InitializeBot()
         {
-            client = new TwitchClient();
-
-            client.Initialize(connectionCredentials, TwitchInfo.ChannelName);
+            client.Initialize(connectionCredentials, _options.ChannelName);
             client.Connect();
 
             client.OnUserJoined += Client_OnUserJoined;
@@ -78,29 +88,25 @@ namespace SonequaBot
             client.OnMessageReceived += Client_OnMessageReceived;
         }
 
-        private void InitializeBotCommands()
-        {
-            //BotCommands.Add(new CommandHi()); Not needed in twitch
-            BotCommands.Add(new CommandJava());;
-            BotCommands.Add(new CommandPhp());
-            BotCommands.Add(new CommandDevastante());
-            BotCommands.Add(new CommandSlap());
-            BotCommands.Add(new CommandDiceRoll());
-        }
-
         private void Client_OnUserLeft(object sender, TwitchLib.Client.Events.OnUserLeftArgs e)
         {
             ConnectedUsers.Remove(e.Username);
+
+            _logger.LogWarning($"The user left: {e.Username}");
+            _logger.LogWarning($"Total user on channel: {ConnectedUsers.Count}");
         }
 
         private void Client_OnUserJoined(object sender, TwitchLib.Client.Events.OnUserJoinedArgs e)
         {
             ConnectedUsers.Add(e.Username, new ConnectedUser(e.Username));
+
+            _logger.LogWarning($"New user on channel: {e.Username}");
+            _logger.LogWarning($"Total user on channel: {ConnectedUsers.Count}");
         }
 
         private void Client_OnConnected(object sender, TwitchLib.Client.Events.OnConnectedArgs e)
         {
-            client.SendMessage(TwitchInfo.ChannelName, $"Hi to everyone. I am Sonequabot and I am alive. Again.");
+            client.SendMessage(_options.ChannelName, $"Hi to everyone. I am Sonequabot and I am alive. Again.");
         }
 
         private async void Client_OnMessageReceived(object sender, TwitchLib.Client.Events.OnMessageReceivedArgs e)
@@ -114,7 +120,7 @@ namespace SonequaBot
                         switch (true)
                         {
                             case true when command is IResponseMessage commandMessage:
-                                client.SendMessage(TwitchInfo.ChannelName, commandMessage.GetMessage(e));
+                                client.SendMessage(_options.ChannelName, commandMessage.GetMessage(e));
                                 break;
 
                             case true when command is IResponseVisual commandVisual:
