f97b196b4471e33dd9b72fac03bd0cadbbe7424b
devdanetra <devdanetra@outlook.com>
2020-10-16 11:49:44 +0200

    added commands Exceptions.
    simplified the way to show exceptions to user.

---
+++ SonequaBot/Commands/Exceptions/CommandExceptions.cs
@@ -0,0 +1,24 @@
+using System;
+
+namespace SonequaBot.Commands.Exceptions
+{
+    public class CommandException : Exception
+        {
+        public CommandException(string message, string input,string command = "default"){
+            message = message.Replace("%input%",input);
+            message = message.Replace("%command%",command);
+            Message = message;
+        }
+
+        public override string Message{get;}
+
+        // %input% refers to the command sent by the user
+        // %command% refers to the command suggested by the bot.
+
+        public static string CommandNotValid = "%input% does not exists.";
+        public static string CommandNotValidSuggest = "%input% does not exists. (Did you mean to use \"%command%\" ?)";
+        public static string CommandNeedsStringArgument = "%command% command needs a target (ex. %command% Valentino).";
+        public static string CommandUnavailable = "%command% is currently unavailable.";
+        public static string CommandNeedsNumberArgument = "%command% needs an argument (ex %command% 3).";
+    }
+}
