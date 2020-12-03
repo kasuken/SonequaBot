e486a8c6a73323b5e22191dbffd46b94b7d4f1f9
Francesco Danti <fdanti@gmail.com>
2020-12-03 10:21:08 +0100

    Add iResponseDiscord -> HiCommand

--- SonequaBot.Shared/Commands/CommandHi.cs
+++ SonequaBot.Shared/Commands/CommandHi.cs
@@ -1,15 +1,22 @@
+using System.Threading.Tasks;
+using DSharpPlus.CommandsNext;
 using SonequaBot.Commands.Interfaces.Responses;
 using TwitchLib.Client.Events;
 
 namespace SonequaBot.Commands
 {
-    public class CommandHi : CommandBase, IResponseMessage
+    public class CommandHi : CommandBase, IResponseMessage, IResponseDiscord
     {
         protected override string ActivationCommand => "hi";
 
         public string GetMessage(OnMessageReceivedArgs e)
         {
             return $"Hey there {e.ChatMessage.DisplayName}.";
+        }
+
+        public async Task DiscordCommand(CommandContext ctx, string name)
+        {
+            await ctx.RespondAsync($"👋 Hi, {ctx.User.Mention}!");
         }
     }
 }
