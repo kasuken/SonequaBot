437fa684145ebedd2bff381deba92c3347471c6a
Francesco Danti <fdanti@gmail.com>
2021-01-25 13:43:35 +0100

    Add !bannotutti

---
+++ SonequaBot.Shared/Commands/CommandBannotutti.cs
@@ -0,0 +1,15 @@
+﻿using SonequaBot.Shared.Commands.Interfaces.Responses;
+
+namespace SonequaBot.Shared.Commands
+{
+    public class CommandBannotutti : CommandBase, IResponseImage
+    {
+        protected override string ActivationCommand => "!bannotutti";
+
+        public string GetImageEvent(CommandSource source)
+        {
+            return
+                "https://raw.githubusercontent.com/kasuken/SonequaBot/master/SonequaBot.Web/wwwroot/img/bannotutti.jpg";
+        }
+    }
+}
