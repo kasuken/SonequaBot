7f9f7a562e376b007c6adc657a6338c0be5e7a44
Alessandro Di Maria <alessandrodimaria3@gmail.com>
2021-02-07 15:27:06 +0100

    Command !choc use .gif instead .mp4

--- SonequaBot.Shared/Commands/CommandChoc.cs
+++ SonequaBot.Shared/Commands/CommandChoc.cs
@@ -2,13 +2,13 @@
 
 namespace SonequaBot.Shared.Commands
 {
-    public class CommandChoc : CommandBase, IResponseVideo
+    public class CommandChoc : CommandBase, IResponseImage
     {
         protected override string ActivationCommand => "!choc";
 
-        public string GetVideoEvent(CommandSource source)
+        public string GetImageEvent(CommandSource source)
         {
-            return "https://raw.githubusercontent.com/kasuken/SonequaBot/master/SonequaBot.Web/wwwroot/img/choc.mp4";
+            return "https://raw.githubusercontent.com/kasuken/SonequaBot/master/SonequaBot.Web/wwwroot/img/choc.gif";
         }
     }
 }
