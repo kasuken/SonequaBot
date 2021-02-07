22cefa1ebf650900c5f8cae410d5f2f32bc7ce02
Alessandro Di Maria <66570558+ImAl3x03@users.noreply.github.com>
2021-02-07 15:05:58 +0100

    Fix command !choc (#52)

--- SonequaBot.Shared/Commands/CommandChoc.cs
+++ SonequaBot.Shared/Commands/CommandChoc.cs
@@ -2,18 +2,13 @@
 
 namespace SonequaBot.Shared.Commands
 {
-    public class CommandChoc : CommandBase, IResponseImage, IResponseAudio
+    public class CommandChoc : CommandBase, IResponseVideo
     {
         protected override string ActivationCommand => "!choc";
 
-        public string GetImageEvent(CommandSource source)
+        public string GetVideoEvent(CommandSource source)
         {
             return "https://raw.githubusercontent.com/kasuken/SonequaBot/master/SonequaBot.Web/wwwroot/img/choc.mp4";
-        }
-
-        public string GetAudioEvent(CommandSource source)
-        {
-            return null;
         }
     }
 }
