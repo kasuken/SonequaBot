29bed22c310924f814a8d712283c37da851c29da
Francesco Danti <fdanti@gmail.com>
2021-03-15 13:18:54 +0100

    Add !bergarambo command (#58)

---
+++ SonequaBot.Shared/Commands/CommandBergarambo.cs
@@ -0,0 +1,14 @@
+﻿using SonequaBot.Shared.Commands.Interfaces.Responses;
+
+namespace SonequaBot.Shared.Commands
+{
+    public class CommandBergarambo : CommandBase, IResponseImage
+    {
+        protected override string ActivationCommand => "!bergarambo";
+
+        public string GetImageEvent(CommandSource source)
+        {
+            return "https://raw.githubusercontent.com/kasuken/SonequaBot/master/SonequaBot.Web/wwwroot/img/bergarambo.gif";
+        }
+    }
+}
