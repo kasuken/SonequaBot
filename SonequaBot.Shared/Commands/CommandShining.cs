79a9d53284a27b9bf2ad0b2029e615714221d5f8
Francesco Danti <fdanti@gmail.com>
2021-04-06 10:43:31 +0200

    Add !shining (#60)

---
+++ SonequaBot.Shared/Commands/CommandShining.cs
@@ -0,0 +1,17 @@
+﻿using SonequaBot.Shared.Commands.Interfaces;
+using SonequaBot.Shared.Commands.Interfaces.Responses;
+
+namespace SonequaBot.Shared.Commands
+{
+    public class CommandShining : CommandBase, IResponseImage
+    {
+        protected override string ActivationCommand => "!shining";
+
+        protected override CommandActivationComparison ActivationComparison => CommandActivationComparison.Contains;
+
+        public string GetImageEvent(CommandSource source)
+        {
+            return "https://raw.githubusercontent.com/kasuken/SonequaBot/master/SonequaBot.Web/wwwroot/img/shining.gif";
+        }
+    }
+}
