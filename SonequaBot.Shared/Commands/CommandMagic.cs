c1335723a5d9bb926872010d970e37bbfb534a2c
valentinotheworld <70565740+valentinotheworld@users.noreply.github.com>
2021-04-08 15:42:35 +0200

    Added !magic command (#61)
    Co-authored-by: Valentino Messina <valentino.messina@gpams.it>

---
+++ SonequaBot.Shared/Commands/CommandMagic.cs
@@ -0,0 +1,17 @@
+﻿using SonequaBot.Shared.Commands.Interfaces;
+using SonequaBot.Shared.Commands.Interfaces.Responses;
+
+namespace SonequaBot.Shared.Commands
+{
+    public class CommandMagic : CommandBase, IResponseImage
+    {
+        protected override string ActivationCommand => "!magic";
+
+        protected override CommandActivationComparison ActivationComparison => CommandActivationComparison.Contains;
+
+        public string GetImageEvent(CommandSource source)
+        {
+            return "https://raw.githubusercontent.com/kasuken/SonequaBot/master/SonequaBot.Web/wwwroot/img/magic.gif";
+        }
+    }
+}
