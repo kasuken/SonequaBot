36cd7ec8da598c614f7fe7b484ef643e4558988f
Valentino Messina <valentino.messina@gpams.it>
2021-04-08 16:51:05 +0200

    added !ramborun command

---
+++ SonequaBot.Shared/Commands/CommandRamboRun.cs
@@ -0,0 +1,17 @@
+﻿using SonequaBot.Shared.Commands.Interfaces;
+using SonequaBot.Shared.Commands.Interfaces.Responses;
+
+namespace SonequaBot.Shared.Commands
+{
+    public class CommandRamboRun : CommandBase, IResponseImage
+    {
+        protected override string ActivationCommand => "!ramborun";
+
+        protected override CommandActivationComparison ActivationComparison => CommandActivationComparison.Contains;
+
+        public string GetImageEvent(CommandSource source)
+        {
+            return "https://raw.githubusercontent.com/kasuken/SonequaBot/master/SonequaBot.Web/wwwroot/img/ramborun.gif";
+        }
+    }
+}
