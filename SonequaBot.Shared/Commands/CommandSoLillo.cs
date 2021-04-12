bb5d972254cb52f76ef0c98990449b8181977c32
valentinotheworld <70565740+valentinotheworld@users.noreply.github.com>
2021-04-12 12:08:25 +0200

    Added !solillo command (#63)
    Co-authored-by: Valentino Messina <valentino.messina@gpams.it>

---
+++ SonequaBot.Shared/Commands/CommandSoLillo.cs
@@ -0,0 +1,17 @@
+﻿using SonequaBot.Shared.Commands.Interfaces;
+using SonequaBot.Shared.Commands.Interfaces.Responses;
+
+namespace SonequaBot.Shared.Commands
+{
+    public class CommandSoLillo : CommandBase, IResponseImage
+    {
+        protected override string ActivationCommand => "!solillo";
+
+        protected override CommandActivationComparison ActivationComparison => CommandActivationComparison.Contains;
+
+        public string GetImageEvent(CommandSource source)
+        {
+            return "https://raw.githubusercontent.com/kasuken/SonequaBot/master/SonequaBot.Web/wwwroot/img/solillo.gif";
+        }
+    }
+}
