58cdb2fe229dbf6c837ecda2a203f38100d07294
Th3Wall <mandellidavide.95@gmail.com>
2021-10-18 16:04:00 +0200

    Added command rovescia

---
+++ SonequaBot.Shared/Commands/CommandRovescia.cs
@@ -0,0 +1,14 @@
+﻿using SonequaBot.Shared.Commands.Interfaces.Responses;
+
+namespace SonequaBot.Shared.Commands
+{
+    public class CommandBuco : CommandBase, IResponseImage
+    {
+        protected override string ActivationCommand => "!rovescia";
+
+        public string GetImageEvent(CommandSource source)
+        {
+            return "https://raw.githubusercontent.com/kasuken/SonequaBot/master/SonequaBot.Web/wwwroot/img/rovescia.jpg";
+        }
+    }
+}
