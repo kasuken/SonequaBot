73c5930424919c87e1d5c83b4bd39130fa41de9a
Th3Wall <mandellidavide.95@gmail.com>
2021-10-20 13:51:28 +0200

    Added command zingari

---
+++ SonequaBot.Shared/Commands/CommandZingari.cs
@@ -0,0 +1,15 @@
+﻿using SonequaBot.Shared.Commands.Interfaces.Responses;
+
+namespace SonequaBot.Shared.Commands
+{
+    public class CommandZingari : CommandBase, IResponseImage
+    {
+        protected override string ActivationCommand => "!zingari";
+
+        public string GetImageEvent(CommandSource source)
+        {
+            return
+                "https://raw.githubusercontent.com/kasuken/SonequaBot/master/SonequaBot.Web/wwwroot/img/zingari.jpg";
+        }
+    }
+}
